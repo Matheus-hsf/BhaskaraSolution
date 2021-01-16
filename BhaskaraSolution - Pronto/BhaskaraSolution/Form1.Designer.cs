a546e98fdcf75380b69884831a846dc6d06040d2
Matheus-hsf <62724614+Matheus-hsf@users.noreply.github.com>
2021-01-16 14:06:28 -0300

    Treinando C# - BhaskaraSolution
    Solução para resolver e mostrar os processos da fórmula de Bhaskara.
    Feito no primeiro ano da faculdade.

---
+++ BhaskaraSolution - Pronto/BhaskaraSolution/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Bhaskár
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tex1 = new System.Windows.Forms.TextBox();
+            this.tex2 = new System.Windows.Forms.TextBox();
+            this.tex3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tex4 = new System.Windows.Forms.TextBox();
+            this.tex5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.btnPassoApasso = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tex1
+            // 
+            this.tex1.Location = new System.Drawing.Point(23, 34);
+            this.tex1.Name = "tex1";
+            this.tex1.Size = new System.Drawing.Size(31, 20);
+            this.tex1.TabIndex = 0;
+            // 
+            // tex2
+            // 
+            this.tex2.Location = new System.Drawing.Point(187, 34);
+            this.tex2.Name = "tex2";
+            this.tex2.Size = new System.Drawing.Size(29, 20);
+            this.tex2.TabIndex = 1;
+            // 
+            // tex3
+            // 
+            this.tex3.Location = new System.Drawing.Point(339, 34);
+            this.tex3.Name = "tex3";
+            this.tex3.Size = new System.Drawing.Size(27, 20);
+            this.tex3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(154, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tex4
+            // 
+            this.tex4.Location = new System.Drawing.Point(82, 185);
+            this.tex4.Name = "tex4";
+            this.tex4.Size = new System.Drawing.Size(55, 20);
+            this.tex4.TabIndex = 4;
+            // 
+            // tex5
+            // 
+            this.tex5.Location = new System.Drawing.Point(261, 185);
+            this.tex5.Name = "tex5";
+            this.tex5.Size = new System.Drawing.Size(55, 20);
+            this.tex5.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.3F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.3F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(189, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.3F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(340, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "c";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.3F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(91, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "X1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.3F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(270, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "X2";
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.Location = new System.Drawing.Point(12, 243);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(75, 23);
+            this.btnInfo.TabIndex = 11;
+            this.btnInfo.Text = "Informação";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // btnPassoApasso
+            // 
+            this.btnPassoApasso.Location = new System.Drawing.Point(450, 116);
+            this.btnPassoApasso.Name = "btnPassoApasso";
+            this.btnPassoApasso.Size = new System.Drawing.Size(75, 23);
+            this.btnPassoApasso.TabIndex = 12;
+            this.btnPassoApasso.Text = "Começo";
+            this.btnPassoApasso.UseVisualStyleBackColor = true;
+            this.btnPassoApasso.Click += new System.EventHandler(this.btnPassoApasso_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(396, 154);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "x1 - passo1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(396, 230);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "x1 - Resposta";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(396, 191);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "x1 - passo 2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(497, 154);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 23);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "x2 - passo 1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(497, 192);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(85, 23);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "x2 - passo 2";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(497, 230);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(85, 23);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "x2 - Resposta";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(422, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(130, 25);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Como Fazer";
+            // 
+            // Bhaskár
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 278);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnPassoApasso);
+            this.Controls.Add(this.btnInfo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tex5);
+            this.Controls.Add(this.tex4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tex3);
+            this.Controls.Add(this.tex2);
+            this.Controls.Add(this.tex1);
+            this.Name = "Bhaskár";
+            this.Text = "Bhaskara Solution";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tex1;
+        private System.Windows.Forms.TextBox tex2;
+        private System.Windows.Forms.TextBox tex3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tex4;
+        private System.Windows.Forms.TextBox tex5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.Button btnPassoApasso;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ BhaskaraSolution - Pronto/BhaskaraSolution/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Bhaskár : Form
+    {
+        public Bhaskár()
+        {
+            InitializeComponent();
+        }
+        double vlaorX1 = 0;
+        double vlaorX2 = 0;
+        double raiz1 = 0;
+        double raiz2 = 0;
+        double a = 0;
+        double b = 0;
+        double c = 0;
+        double x1 = 0;
+        double x2 = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(tex1.Text);
+            b = Convert.ToDouble(tex2.Text);
+            c = Convert.ToDouble(tex3.Text);
+
+            x1 =Math.Sqrt(b * b - (4 * a * c));
+            raiz1 = Convert.ToDouble(x1);
+            x1 = -1 * b + x1;
+            vlaorX1 = Convert.ToDouble(x1);
+            x1 = x1 / (2 * a);
+
+            x2 = Math.Sqrt(b * b - (4 * a * c));
+            raiz2 = Convert.ToDouble(x2);
+            x2 = -1 * b - x2;
+            vlaorX2 = Convert.ToDouble(x2);
+            x2 = x2 / (2 * a);
+
+            tex4.Text = Convert.ToString(x1);
+            tex5.Text = Convert.ToString(x2);
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Informe o (a), o (b) e o (c) da equação. Exemplo 3x² + 5x - 14; Neste exemplo o a = 3, o b = 5 e o c = -14.     Se uma das respostas estiver em forma de fração ele sera apresentada de forma decimal. Se a mensagem (NaN) provavelmente o número da raiz deu um valor negativo ou não exato.");
+        }
+
+        private void btnPassoApasso_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show( "-" + (tex2.Text) + "+-" + "Raiz de:" + "(" + tex2.Text + "*" + tex2.Text + "-" + "4" + "*" + tex1.Text + "*" + tex3.Text + ")" );
+        }
+
+        
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show(vlaorX1.ToString() + "/" + "2*" + tex1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("-" + (tex2.Text) + "+" + raiz1.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(x1.ToString());
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("-" + (tex2.Text) + "-" + raiz2.ToString());
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(vlaorX2.ToString() + "/" + "2*" + tex1.Text);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(x2.ToString());
+        }
+       
+    }
+}
